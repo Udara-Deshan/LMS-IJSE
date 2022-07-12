a75845c8667d307ac0ce76e57c6994d2843ca000
Udara Deshan <udaradeshan.ud@gmail.com>
2022-07-12 10:31:18 +0530

    set form Controller

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,8 +1,60 @@
 package controller;
 
+import dto.StudentDTO;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 /**
  * @author : Udara Deshan <udaradeshan.ud@gmail.com>
  * @since : 7/12/2022
  **/
 public class StudentController {
+    @FXML
+    private TableView<StudentDTO> tblStudent;
+
+    @FXML
+    private TextField txtSid;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtNIC;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    void OnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdate(ActionEvent event) {
+
+    }
+
 }
---
+++ src/entity/Student.java
@@ -0,0 +1,14 @@
+package entity;
+
+/**
+ * @author : Udara Deshan <udaradeshan.ud@gmail.com>
+ * @since : 7/12/2022
+ **/
+public class Student {
+    String id;
+    String name;
+    String email;
+    String contact;
+    String address;
+    String nic;
+}
