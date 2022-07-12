8513a980f93c0c788e528606075f684e57083a64
Udara Deshan <udaradeshan.ud@gmail.com>
2022-07-12 11:16:01 +0530

    SET DAO factory and create db

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,8 +1,29 @@
 package dao;
 
+import dao.student.Impl.StudentDAOImpl;
+
 /**
  * @author : Udara Deshan <udaradeshan.ud@gmail.com>
  * @since : 7/12/2022
  **/
 public class DAOFactory {
+    public enum DAOType {
+        STUDENT
+    }
+    private static DAOFactory dAOFactory;
+
+    public static DAOFactory getDAOFactory() {
+        return (dAOFactory != null) ? dAOFactory : (dAOFactory = new DAOFactory());
+    }
+
+    public SuperDAO getSuperDAO(DAOType dAOType) {
+        switch (dAOType) {
+            case STUDENT:
+                return new StudentDAOImpl();
+
+            default:
+                return null;
+        }
+    }
+
 }
